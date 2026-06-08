2686acd7e343022f4943c26c2c9b855f8bd863a2
Lee Subin <99sbin@gmail.com>
2026-06-08 10:45:48 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/subin/goozzu/GoozzuApplication.java
@@ -0,0 +1,13 @@
+package com.subin.goozzu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoozzuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoozzuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subin/goozzu/GoozzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subin.goozzu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoozzuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
